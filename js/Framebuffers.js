53d11da8cef01f34dcd8051f57213ec14c2834c9
gustavo <graushf@gmail.com>
2018-02-15 13:41:06 +0100

    Fixing Downsample Pass

--- js/Framebuffers.js
+++ js/Framebuffers.js
@@ -47,7 +47,14 @@ function createBackBufferHalf() {
     backBufferHalf = gl.createFramebuffer();
     gl.bindFramebuffer(gl.FRAMEBUFFER, backBufferHalf);
 
-    var texture = createAndSetupTexture();
+    var texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+
+    // Set up texture so we can render any size image and so we are working with pixels.
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
     textureBackBufferHalf = texture;
 
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.viewportWidth/downsampleCoefficient, gl.viewportHeight/downsampleCoefficient, 0, gl.RGBA, gl.FLOAT, null);
